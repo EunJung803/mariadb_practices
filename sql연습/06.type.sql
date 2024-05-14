71f4ec253d0f8fa97cb1513df37218e39d47492f
EunJung803 <aimee3322@gmail.com>
2024-05-14 17:04:38 +0900

    file name refactor, sql 연습 (날짜함수, type, 집계, 조인, outer조인)

---
+++ sql연습/06.type.sql
@@ -0,0 +1,15 @@
+-- cast
+select '12345' + 10, cast('12345' as int) + 10 from dual;
+select date_format(cast('2013-01-09' as date), '%Y년 %m월 %d일') from dual;
+select cast(cast(1-2 as unsigned) as signed) from dual;
+select cast(cast(1-2 as unsigned) as int) from dual;
+select cast(cast(1-2 as unsigned) as integer) from dual;
+
+-- type
+-- 문자 : varchar, char, text, CLOB
+-- 정수 : meduim int, int(sigend, integer), unsigned, big int
+-- 실수 : float, double
+-- 시간 : date, datetime
+-- LOB : CLOB(Charracter Large OBject), BLOB(Binary Large OBject)
+
+
